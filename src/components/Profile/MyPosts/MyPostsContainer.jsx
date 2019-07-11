c440fe140eb985845ca40d3e39ff330f27aacbb9
amadeus <es_252@mail.ru>
2019-07-11 17:33:38 +0600

    added react-redux

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import {addPostActionCreator, updateNewsPostTextActionCreator}  from '../../../redux/profile-reducer.js';
 import MyPosts from './MyPosts';
+import {connect} from 'react-redux';
 
-const MyPostsContainer = (props) => {
-
-  let state = props.store.getState();
-
-  let addPost = () => {
-    props.store.dispatch(addPostActionCreator());
+const mapStateToProps = (state) => {
+  return {
+    posts: state.profilePage.posts,
+    newPostText: state.profilePage.newPostText
   }
-
-  let onChangePost = (text) => {
-    let action = updateNewsPostTextActionCreator(text);
-    props.store.dispatch(action);
-  }
-
-  return (
-    <MyPosts updateNewPostText={onChangePost} addPost={addPost}
-             posts={state.profilePage.posts}
-             newPostText={state.profilePage.newPostText}/>
-  );
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateNewPostText: (text) => {
+      let action = updateNewsPostTextActionCreator(text);
+      dispatch(action);
+    },
+
+    addPost: () => {
+      dispatch(addPostActionCreator());
+    }
+
+
+  };
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts);
 
 export default MyPostsContainer;
--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
-let renderEntireTree = (state) => {
+let renderEntireTree = () => {
   ReactDOM.render(
     <BrowserRouter>
-      <App state={state} dispatch={store.dispatch.bind(store)} store={store}/>
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>, document.getElementById('root'));
 }
 
 renderEntireTree(store.getState());
 
 store.subscribe(() => {
-  let state = store.getState()
-  renderEntireTree(state);
+  renderEntireTree();
 });
 
 
