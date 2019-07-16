0418ea0a671f2b67ae8ea0b99d07cb52330ce63d
amadeus <es_252@mail.ru>
2019-07-16 19:40:59 +0600

    added fetch

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,12 +3,14 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET-TOTAL-USERS-COUNT';
+const TOGGLE_IS_FETCHING = 'TOGGLE-IS-FETCHING';
 
 let initialState = {
   users: [],
   pageSize: 5,
   totalUsersCount: 0,
-  currentPage: 1
+  currentPage: 1,
+  isFetching: false
 };
 
 
@@ -49,6 +51,11 @@ const usersReducer = (state = initialState, action) => {
           ...state,
           totalUsersCount: action.count
         }
+    case TOGGLE_IS_FETCHING:
+        return {
+          ...state,
+          isFetching: action.isFetching
+        }
     default:
         return state;
   }
@@ -83,6 +90,13 @@ export const setUsersTotalCountAC = (totalUsersCount) => {
   return {
     type: SET_TOTAL_USERS_COUNT,
     count: totalUsersCount
+  }
+}
+
+export const toggleIsFetchingAC = (isFetching) => {
+  return {
+    type: TOGGLE_IS_FETCHING,
+    isFetching
   }
 }
 
