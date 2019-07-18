7a1e3ea8cf8740ee37a35108e297f2ae27427a19
amadeus <es_252@mail.ru>
2019-07-18 18:14:01 +0600

    DAL

---
+++ src/api/api.js
@@ -0,0 +1,18 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    "API-KEY": "78383281-de66-43e3-b509-170967581e18"
+  }
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+      .then(response => {
+          return response.data
+       });
+  }
+}
